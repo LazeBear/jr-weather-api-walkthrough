805c9a86b78cc28d7f1950fa6e413f4892dc5bfa
Mason Xiong <mason.xiong@yahoo.com>
2019-01-19 15:09:21 +1000

    add dotenv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 const routes = require('./routes');
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(routes);
 
-app.listen(3000, () => console.log(`app listen on port 3000`));
+app.listen(PORT, () => console.log(`app listen on port ${PORT}`));
--- src/routes/weather.js
+++ src/routes/weather.js
@@ -2,13 +2,14 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
+const APPID = process.env.APPID;
 
 router.get('/:cc/:city', (req, res) => {
   const { cc, city } = req.params;
 
   axios
     .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=11968cd850d06577bf436eec5f63a333`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=${APPID}`
     )
     .then(response => {
       res.send(response.data);
