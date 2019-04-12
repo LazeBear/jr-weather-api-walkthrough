aa05afd567de07b896bbb41669d3a78079b4a5bc
Mason Xiong <mason.xiong@yahoo.com>
2019-04-12 23:33:25 +1000

    update route

--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const axios = require('../utils/axios');
 const weather = require('../models/Weather');
 const responseFormatter = require('../utils/responseFormatter');
 const countryValidator = require('../middlewares/countryValidator');
 
 const router = express.Router();
-const APPID = process.env.APPID;
 
-router.get('/:cc/:city', countryValidator, (req, res, next) => {
+router.get('country/:cc/city/:city', countryValidator, (req, res, next) => {
   const { cc, city } = req.params;
   const weatherType = req.query.weatherType;
   weather
