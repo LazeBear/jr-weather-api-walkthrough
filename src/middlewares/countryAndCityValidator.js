42ebd87493d64efb90d6db7246af53ab36146c76
Mason Xiong <mason.xiong@yahoo.com>
2019-04-12 23:47:36 +1000

    update server

--- src/middlewares/countryAndCityValidator.js
+++ src/middlewares/countryAndCityValidator.js
@@ -1,7 +1,15 @@
 const { getCode, getName } = require('country-list');
 const responseFormatter = require('../utils/responseFormatter');
 module.exports = (req, res, next) => {
-  const { cc } = req.params;
+  const { cc, city } = req.query;
+  if (!cc || !city) {
+    return responseFormatter(
+      res,
+      400,
+      'country or city parameter is missing',
+      null
+    );
+  }
   if (cc.length === 2) {
     if (getName(cc)) {
       return next();
--- src/routes/weather.js
+++ src/routes/weather.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const weather = require('../models/Weather');
 const responseFormatter = require('../utils/responseFormatter');
-const countryValidator = require('../middlewares/countryValidator');
+const countryAndCityValidator = require('../middlewares/countryAndCityValidator');
 
 const router = express.Router();
 
-router.get('country/:cc/city/:city', countryValidator, (req, res, next) => {
-  const { cc, city } = req.params;
-  const weatherType = req.query.weatherType;
+router.get('/', countryAndCityValidator, (req, res, next) => {
+  const { cc, city, weatherType } = req.query;
   weather
     .getData(city, cc, weatherType)
     .then(response => responseFormatter(res, 200, null, response))
